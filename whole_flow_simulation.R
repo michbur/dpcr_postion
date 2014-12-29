cdbfc9c8ace4dca46d74f14acb6839758d3ab3b3
michbur <michalburdukiewicz@gmail.com>
2014-12-29 23:29:18 +0100

    removed old code

--- whole_flow_simulation.R
+++ whole_flow_simulation.R
@@ -1,15 +1,3 @@
-#random clusters
-whole_flow <- sample(c(0, 1))
-for (i in 2L:flow_length) {
-  p_bigger <- sample(75L:100, 1)/100
-  probs_01 <- if(c3[i - 1] == 0) {
-    c(p_bigger, 1 - p_bigger)
-  } else {
-    c(1 - p_bigger, p_bigger)
-  }
-  whole_flow[i] <- sample(c(0, 1), 1, prob = probs_01)
-}
-
 #attempt 2 - simulate whole flow and reorder droplets to get five cases
 whole_flow <- as.vector(slot(sim_ddpcr(50, 200, 2000, n_exp = 1, dube = TRUE), ".Data"))
 
