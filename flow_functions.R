eb020c6d6cd3b0b9e84c74f57f0403d5709c8002
michbur <michalburdukiewicz@gmail.com>
2014-12-30 10:16:44 +0100

    created flow_function.R for better simulations of flows

---
+++ flow_functions.R
@@ -0,0 +1,62 @@
+#a set of functions for simulating various flows
+
+generate_flow <- function(in_flow, probs_next) {
+  binary_flow <- in_flow != 0
+  #resample droplets
+  pos_ids <- sample(which(binary_flow))
+  neg_ids <- sample(which(!binary_flow))
+  
+  out_flow <- c()
+  first_droplet_pos <- sample(c(TRUE, FALSE), 1)
+  if(first_droplet_pos) {
+    out_flow[1] <- in_flow[pos_ids[1]]
+    pos_ids <- pos_ids[-1]
+  } else {
+    out_flow[1] <- in_flow[neg_ids[1]]
+    neg_ids <- neg_ids[-1]
+  }
+  
+  while(length(out_flow) < length(in_flow)) {
+    #if clause below takes care of situation in which we don't have more positive/negative droplets 
+    #to fill the flow
+    if(length(pos_ids) != 0 && length(neg_ids) != 0) {
+      next_droplet <- sample(c(TRUE, FALSE), 1, prob = probs_next(in_flow, out_flow))
+      if(next_droplet) {
+        out_flow[length(out_flow) + 1] <- in_flow[pos_ids[1]]
+        pos_ids <- pos_ids[-1]
+      } else {
+        out_flow[length(out_flow) + 1] <- in_flow[neg_ids[1]]
+        neg_ids <- neg_ids[-1]
+      }
+    } else {
+      if(length(pos_ids) != 0) {
+        out_flow[length(out_flow) + 1] <- in_flow[pos_ids[1]]
+        pos_ids <- pos_ids[-1]
+      } else {
+        out_flow[length(out_flow) + 1] <- in_flow[neg_ids[1]]
+        neg_ids <- neg_ids[-1]
+      }
+    }
+  }
+  out_flow
+}
+
+probs_URC <- function(in_flow, out_flow) {
+  #URC need only out_flow, in_flow is here for sake of compatibility
+  #by manipulating these probabilities clusters can be shorter or longer. The more p_bigger
+  #is closer to 0.5, the shorter clusters
+  p_bigger <- sample(75L:100, 1)/100
+  probs_next <- if(out_flow[length(out_flow)] != 0) {
+    c(p_bigger, 1 - p_bigger)
+  } else {
+    c(1 - p_bigger, p_bigger)
+  }
+  probs_next
+}
+
+probs_PC <- function(in_flow, out_flow) {
+  #p_pos (probability of positive droplet) - decreases with the number of droplets
+  p_pos <- (1 - length(out_flow)/length(in_flow))*0.8
+  #p_pos should be a function of length, on it depend how strong is correlation
+  c(p_pos, 1 - p_pos)
+}
--- whole_flow_simulation.R
+++ whole_flow_simulation.R
@@ -1,5 +1,7 @@
+library(dpcR)
+source("flow_functions.R")
 #attempt 2 - simulate whole flow and reorder droplets to get five cases
-whole_flow <- as.vector(slot(sim_ddpcr(50, 200, 2000, n_exp = 1, dube = TRUE), ".Data"))
+whole_flow <- as.vector(slot(sim_ddpcr(8000, 20000, 2000, n_exp = 1, dube = TRUE), ".Data"))
 
 #case 1: complete randomness (RAND)
 #we dont have to do sample again, but for sake of randomness
@@ -10,90 +12,8 @@ c1 <- sample(whole_flow)
 
 
 #case 3: unregular clusters (URC)
-binary_flow <- whole_flow != 0
-pos_ids <- sample(which(binary_flow))
-neg_ids <- sample(which(!binary_flow))
+c3 <- generate_flow(whole_flow, probs_URC)
 
-c3 <- c()
-first_droplet_pos <- sample(c(TRUE, FALSE), 1)
-if(first_droplet_pos) {
-  c3[1] <- whole_flow[pos_ids[1]]
-  pos_ids <- pos_ids[-1]
-} else {
-  c3[1] <- whole_flow[neg_ids[1]]
-  neg_ids <- neg_ids[-1]
-}
-
-while(length(c3) < length(whole_flow)) {
-  #by manipulating these probabilities clusters can be shorter or longer. The more p_bigger
-  #is closer to 0.5, the shorter clusters
-  p_bigger <- sample(75L:100, 1)/100
-  probs_next <- if(c3[length(c3)] != 0) {
-    c(p_bigger, 1 - p_bigger)
-  } else {
-    c(1 - p_bigger, p_bigger)
-  }
-  
-  #this if takes care of situation in which we don't have more positive/negative droplets 
-  #to fill the flow
-  if(length(pos_ids) != 0 && length(neg_ids) != 0) {
-    next_droplet <- sample(c(TRUE, FALSE), 1, prob = probs_next)
-    if(next_droplet) {
-      c3[length(c3) + 1] <- whole_flow[pos_ids[1]]
-      pos_ids <- pos_ids[-1]
-    } else {
-      c3[length(c3) + 1] <- whole_flow[neg_ids[1]]
-      neg_ids <- neg_ids[-1]
-    }
-  } else {
-    if(length(pos_ids) != 0) {
-      c3[length(c3) + 1] <- whole_flow[pos_ids[1]]
-      pos_ids <- pos_ids[-1]
-    } else {
-      c3[length(c3) + 1] <- whole_flow[neg_ids[1]]
-      neg_ids <- neg_ids[-1]
-    }
-  }
-}
 
 #case 4: less probable positive droplet at the end of experiment - position correlation (PC)
-binary_flow <- whole_flow != 0
-pos_ids <- sample(which(binary_flow))
-neg_ids <- sample(which(!binary_flow))
-
-c4 <- c()
-first_droplet_pos <- sample(c(TRUE, FALSE), 1)
-if(first_droplet_pos) {
-  c4[1] <- whole_flow[pos_ids[1]]
-  pos_ids <- pos_ids[-1]
-} else {
-  c4[1] <- whole_flow[neg_ids[1]]
-  neg_ids <- neg_ids[-1]
-}
-
-while(length(c4) < length(whole_flow)) {
-  #p_pos (probability of positive droplet) - decreases with the number of droplets
-  p_pos <- (1 - length(c4)/length(whole_flow))*0.6
-  #p_pos should be a function of length, on it depend how strong is correlation
-  print(p_pos)
-  probs_next <- c(p_pos, 1 - p_pos)
-  
-  if(length(pos_ids) != 0 && length(neg_ids) != 0) {
-    next_droplet <- sample(c(TRUE, FALSE), 1, prob = probs_next)
-    if(next_droplet) {
-      c4[length(c4) + 1] <- whole_flow[pos_ids[1]]
-      pos_ids <- pos_ids[-1]
-    } else {
-      c4[length(c4) + 1] <- whole_flow[neg_ids[1]]
-      neg_ids <- neg_ids[-1]
-    }
-  } else {
-    if(length(pos_ids) != 0) {
-      c4[length(c4) + 1] <- whole_flow[pos_ids[1]]
-      pos_ids <- pos_ids[-1]
-    } else {
-      c4[length(c4) + 1] <- whole_flow[neg_ids[1]]
-      neg_ids <- neg_ids[-1]
-    }
-  }
-}
+c4 <- generate_flow(whole_flow, probs_PC)
